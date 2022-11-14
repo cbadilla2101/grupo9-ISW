73c26717191547fed02df2de7e46aa3d6ea69744
Francisco <francisco.erices1901@alumnos.ubiobio.cl>
2022-11-13 22:00:54 -0300

    Creacion de Modelo,Controlador y Rutas de Usuario(correccion)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 const categoriaRoutes = require('./routes/categorias.routes');
+const usuarioRoutes = require('./routes/usuarios.routes');
 const mantencionRoutes = require('./routes/mantenciones.routes');
 
 const app = express();
@@ -17,6 +18,7 @@ app.options('*', cors());
 
 app.get('/', (req, res) => res.json('API Mantencion de Instalaciones'));
 app.use('/api', categoriaRoutes);
+app.use('/api', usuarioRoutes);
 app.use('/api', mantencionRoutes);
 
 module.exports = app;
